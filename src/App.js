97723cbab50a5b2cffd3f37c96eb8f5ff760fced
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-12-13 20:09:15 +0100

    map, script, keys

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import './Keys.js';
+import keys from './Keys';
+
+const API_KEY = keys.api;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    }
+  }
+  // calling renderMap
+  componentDidMount() {
+    this.renderMap()
+  }
+  // fetchVenues = () => {
+  //   const url = 'https://api.foursquare.com/v2/venues/explore';
+  //   const param = {
+  //     my_id: '',
+  //     my_secret: '',
+  //     query: 'coffee',
+  //     lat: 48.208176,
+  //     lng: 16.373819
+  //   }
+  // }
+  renderMap = () => {
+    const url = "https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3&callback=initMap";
+    runScript(url)
+    window.initMap = this.initMap
+  }
+  // function initMap() {}
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: {lat: 48.208176, lng: 16.373819},
+    zoom: 13
+  })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main>
+        <div id="map"></div>
+      </main>
+    )
   }
 }
-
+function runScript(url) {
+  // var reference = window.document.getElementsByTagName('script')[0];
+  var script = window.document.createElement('script');
+  script.src = url
+  script.async = true
+  script.defer = true
+  document.body.appendChild(script);
+  // reference.insertBefore(script, reference.childNodes[0])
+}
 export default App;
