1c9d3c684def0ec01182b019e4fb96bb2c98279a
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-12-14 00:50:38 +0100

    show markers + infoWindow

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import keys from './Keys';
 import * as myLocations from './locations.json';
 
-const API_KEY = keys.api;
+// console.log(process.env.REACT_APP_xxx);
+
+ const API_KEY =`${process.env.REACT_APP_API_KEY}`
+ const MY_ID =`${process.env.REACT_APP_ID}`
+ const MY_SECRET =`${process.env.REACT_APP_SECRET}`
+
+// const API_KEY = keys.api;
 
 class App extends Component {
   constructor(props) {
@@ -38,27 +44,32 @@ class App extends Component {
   // }
 
   renderMap = () => {
-    const url = "https://maps.googleapis.com/maps/api/js?key=${API}&v=3&callback=initMap";
+    const url = "https://maps.googleapis.com/maps/api/js?key=&v=3&callback=initMap";
     runScript(url)
     window.initMap = this.initMap
   }
 
   // function initMap() {}
   initMap = () => {
+    let anchorShow = this;
+
     const latLng = {lat: 48.208176, lng: 16.373819};
 
     let map = new window.google.maps.Map(document.getElementById('map'), {
       center: latLng,
-      zoom: 13
+      zoom: 13,
+      // styles: styles
     });
 
     const { locations } = this.state;
-    let largeInfoWindow = new window.google.maps.InfoWindow();
 
+    let infoWindow = new window.google.maps.InfoWindow();
+
+     /* Keep state in sync */
+    this.setState({ map, infoWindow });
 
     console.log(locations.default[0]);
     for (let i = 0; i < locations.default.length; i++) {
-
       let position = locations.default[i].location;
       let title = locations.default[i].title;
       let img = locations.default[i].image;
@@ -72,8 +83,29 @@ class App extends Component {
         id_marker: id,
         animation: window.google.maps.Animation.DROP,
       });
+
+      // add event listener when click on marker
+      marker.addListener('mouseover', function () {
+        anchorShow.showInfoWindow(this, infoWindow);
+      });
     }
   }
+
+    showInfoWindow(marker, infowindow) {
+      const { map, infoWindow } = this.state;
+
+      if(infowindow.marker !== marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' +
+          '<h3>' + marker.title  + '</h3>' +
+          '<img width="300" src="' + marker.image + '"/>'
+          + '</div>');
+        infowindow.open(map,marker);
+        infoWindow.addListener('mouseout', function () {
+          infoWindow.setMarker = null;
+        });
+      }
+    }
 
   render() {
     return (
@@ -93,5 +125,6 @@ function runScript(url) {
   script.async = true
   script.defer = true
   document.body.appendChild(script);
+  // document.head.appendChild(script)
   // reference.insertBefore(script, reference.childNodes[0])
 }
